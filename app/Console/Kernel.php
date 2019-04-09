74889ade19f69d6c6ed27359d7c2bc3b8d2848f6
tonggo oscar <tonggooscar@gmail.com>
2019-04-09 16:00:25 +0700

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
+		'\App\Console\Commands\CronJob',
     ];
 
     /**
@@ -26,6 +27,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+		$schedule->command('CronJob:cronjob')
+                 ->everyMinute();
     }
 
     /**
